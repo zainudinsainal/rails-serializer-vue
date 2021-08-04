347e628de56fba43004cda02919e335c2e8f0c13
zainudinsainal <supernova37@gmail.com>
2021-08-05 05:18:15 +0800

    test

--- app/javascript/src/services/pages/components/home/index.vue
+++ app/javascript/src/services/pages/components/home/index.vue
@@ -2,8 +2,8 @@
   <div id="pages">
     <h1>Test</h1>
     <router-link to="/blogs" class="btn btn-default pull-left">Blogs</router-link>
+  <router-view></router-view>
   </div>
-
 </template>
 
 <script>
